37a8b7b4f93339ba3433b01a576b85254f6bfca9
Jacob R. Dale <snicks.phyla0y@icloud.com>
2025-07-12 19:26:57 +0000

    Message x2

--- webserver/handlers/admin.go
+++ webserver/handlers/admin.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/owncast/owncast/core/rtmp"
-	"github.com/owncast/owncast/webserver/handlers/admin"
-	"github.com/owncast/owncast/webserver/handlers/generated"
-	"github.com/owncast/owncast/webserver/router/middleware"
+	"github.com/jacobrdale/owncast/core/rtmp"
+	"github.com/jacobrdale/owncast/webserver/handlers/admin"
+	"github.com/jacobrdale/owncast/webserver/handlers/generated"
+	"github.com/jacobrdale/owncast/webserver/router/middleware"
 )
 
 func Status(w http.ResponseWriter, r *http.Request) {
